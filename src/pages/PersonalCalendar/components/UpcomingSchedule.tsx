d2c819e0afaa04da91f25ae62ac3c70a5f6d05eb
chahyunlee <chahyun023@gmail.com>
2025-11-01 01:00:06 +0900

    style: 디자인 수정(#129)

--- src/pages/PersonalCalendar/components/UpcomingSchedule.tsx
+++ src/pages/PersonalCalendar/components/UpcomingSchedule.tsx
@@ -5,8 +5,8 @@ const UpcomingSchedule = () => {
   return (
     <div className="overflow-hidden p-6 bg-white rounded-xl border shadow-lg border-mainBlue/70">
       <div className="flex gap-2 items-center mb-2">
-        <AlarmClock className="w-4 h-4 text-mainBlue" />
-        <h3 className="text-lg font-semibold text-mainBlue">다가오는 일정</h3>
+        <AlarmClock className="w-4.5 h-4.5 text-mainBlue " />
+        <h3 className="text-lg font-semibold">다가오는 일정</h3>
       </div>
       <UpcomingScheduleList />
     </div>
--- src/pages/TeamCalendar/components/UpcomingTeamSchedule.tsx
+++ src/pages/TeamCalendar/components/UpcomingTeamSchedule.tsx
@@ -3,11 +3,11 @@ import { UpcomingTeamScheduleList } from '@/pages/TeamCalendar/components/Upcomi
 const UpcomingTeamSchedule = () => {
   return (
     <>
-      <div className="overflow-hidden rounded-xl">
-        <div className="flex flex-col gap-2 mb-2">
-          <h3 className="text-lg font-semibold">팀 주간 일정</h3>
-          <UpcomingTeamScheduleList />
+      <div className="overflow-hidden">
+        <div className="flex gap-2 items-center mb-2">
+          <h3 className="text-lg font-semibold mb-2">다가오는 일정</h3>
         </div>
+        <UpcomingTeamScheduleList />
       </div>
     </>
   );
