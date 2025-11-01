38c7093e29fa7967e79aa93dbc08a436f7a029f5
HanseKim <kimbird2002@naver.com>
2025-11-01 19:39:08 +0900

    Fix: Add missing getCalendarEventsResponse import

--- src/apis/services/calendar.ts
+++ src/apis/services/calendar.ts
@@ -13,6 +13,7 @@ import type {
   addCalendarEventResponse,
   addCalendarRecurringEventRequest,
   deleteCalendarRecurringOneEventRequest,
+  getCalendarEventsResponse,
   modifyCalendarEventRequest,
   modifyCalendarEventResponse,
   modifyCalendarRecurringAllEventRequest,
